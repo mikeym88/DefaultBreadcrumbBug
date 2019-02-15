ae4e90b93fa256d6d669f8b9ab7bb36867be20ab
Mike Martinescu <mike.martinescu@canada.ca>
2019-02-15 16:07:40 -0500

    VS .Net Core 2.1 project demonstrating the [DefaultBreadcrumb("ViewData.Title")] bug in the SmartBreadcrumbs NuGet package

---
+++ DefaultBreadcrumbBug/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using DefaultBreadcrumbBug.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using SmartBreadcrumbs;
+
+namespace DefaultBreadcrumbBug.Controllers
+{
+    public class HomeController : Controller
+    {
+        [DefaultBreadcrumb("ViewData.Title")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Breadcrumb("ViewData.Title", CacheTitle = true)]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        [Breadcrumb("ViewData.Title", CacheTitle = true)]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        [Breadcrumb("ViewData.Title", CacheTitle = true)]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Breadcrumb("ViewData.Title", CacheTitle = true)]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
